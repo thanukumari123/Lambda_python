8e4332bbda69ff4e6dbb7aa0b19848cf937ece48
thanukumari123 <kumarithanu324@gmail.com>
2026-04-21 20:32:19 +0530

    Add files via upload

---
+++ lambda function.py
@@ -0,0 +1,6 @@
+add1 = lambda a, b: a + b
+print("Addition:", add1(1, 8))   
+add2 = lambda a, b: a + b / a - b
+print("Expression:", add2(1, 8))   
+mod = lambda a, b: a % b
+print("Modulus:", mod(5, 10))     
